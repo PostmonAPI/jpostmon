b28a27221b7169e8958e722d62dc41e98878394e
Neto Marin <netomarin@gmail.com>
2014-04-17 12:02:18 -0300

    Adicionando licença Apache e Javadoc ao projeto

--- jpostmon/src/main/java/br/com/postmon/jpostmon/App.java
+++ jpostmon/src/main/java/br/com/postmon/jpostmon/App.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 Postmon API (http://postmon.com.br)
+ * https://github.com/PostmonAPI 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package br.com.postmon.jpostmon;
 
 import br.com.postmon.jpostmon.dao.Consultas;
@@ -7,10 +23,12 @@ import br.com.postmon.jpostmon.dao.Endereco;
 /**
  * Classe para demonstrar o uso da API de consulta de CEP
  * @author netomarin
- * TODO mover código para teste e remover essa classe
  */
 public class App 
-{
+{	
+	/*
+	 *  TODO mover esse código para um exemplo separado e remover do client
+	 */
     public static void main( String[] args )
     {
     	Endereco endereco = Postmon.consultar(Consultas.CEP)
